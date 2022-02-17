b5ab10ee07fd4bb6fc8c68dbce9ec8f49dc0cef6
Alice Cutler <acutler0451@gmail.com>
2022-02-16 21:28:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // DELETE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function getCity() {
+	const myCity = document.getElementById('my-city').value;
+	console.log(myCity);
+	return myCity;
+}
