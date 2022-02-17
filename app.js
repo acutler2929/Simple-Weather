f6251bdec1d16226ff914bd3ad8c4746294e1a34
Alice Cutler <acutler0451@gmail.com>
2022-02-17 14:05:03 -0500

    implemented geocoding

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // DELETE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // DELETE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-function getCity() {
+function getWeather() {
 	const myCity = document.getElementById('my-city').value;
+	const stateCode = document.getElementById('state-code').value;
+	const countryCode = document.getElementById('country-code').value;
+	const resultLimit = 1;
 	console.log(myCity);
-	return myCity;
+
+	fetch(
+		`http://api.openweathermap.org/geo/1.0/direct?q=${myCity},${stateCode},${countryCode}&limit=${resultLimit}&appid=${apiKey}`
+	).then((response) => {
+		const myLatLong = response.json();
+		console.log(myLatLong);
+		return myLatLong;
+	});
+
+	// return myWeather;
 }
