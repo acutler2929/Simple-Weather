b43aebc62acee9ae51f9225ff4159da92b0d51d1
Alice Cutler <acutler0451@gmail.com>
2022-02-17 21:51:26 -0500

    got weather data from coordinates

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // DELETE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const apiKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // DELETE ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const myCity = document.getElementById('my-city').value;
+const stateCode = document.getElementById('state-code').value;
+const countryCode = document.getElementById('country-code').value;
+const resultLimit = 1;
 
-function getWeather() {
-	const myCity = document.getElementById('my-city').value;
-	const stateCode = document.getElementById('state-code').value;
-	const countryCode = document.getElementById('country-code').value;
-	const resultLimit = 1;
-	console.log(myCity);
+async function getWeather() {
+	let response = await fetch(
+		`http://api.openweathermap.org/geo/1.0/direct?q=kalamazoo,mi,usa&limit=${resultLimit}&appid=${apiKey}`
+	);
+	const data = await response.json();
+	console.log(data);
+	const myCityLat = data[0].lat;
+	const myCityLon = data[0].lon;
+	console.log(myCityLat, myCityLon);
 
-	fetch(
-		`http://api.openweathermap.org/geo/1.0/direct?q=${myCity},${stateCode},${countryCode}&limit=${resultLimit}&appid=${apiKey}`
-	).then((response) => {
-		const myLatLong = response.json();
-		console.log(myLatLong);
-		return myLatLong;
-	});
-
-	// return myWeather;
+	let myWeather = await fetch(
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${myCityLat}&lon=${myCityLon}&exclude=minutely,alerts&appid=${apiKey}`
+	);
+	const weatherData = await myWeather.json();
+	console.log(weatherData);
 }
