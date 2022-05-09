66fc3b79d5074403773c79b631aec47c84764cdd
Alice Cutler <acutler0451@gmail.com>
2022-05-09 15:30:06 -0400

    still working on it

---
+++ public/public.js
@@ -0,0 +1,66 @@
+'use stirct';
+
+//////////// show the nav tabs:
+function showForecastButtons() {
+	document.getElementById('forecast-buttons').classList.remove('hidden');
+	document
+		.getElementById('current-weather-wrapper')
+		.classList.remove('hidden');
+	// console.log('Display the weather tabs.');
+}
+
+///////////////// DEFAULT display Current Weather- hide Hourly and One Week Weather:
+function selectCurrentWeather() {
+	document
+		.getElementById('current-weather-wrapper')
+		.classList.remove('hidden');
+	document.getElementById('hourly-forecast-wrapper').classList.add('hidden');
+	document
+		.getElementById('one-week-forecast-wrapper')
+		.classList.add('hidden');
+	// console.log('Show just the currrent weather.');
+}
+
+/////////////////// display Hourly Weather- and hide Current and One Week Weather:
+function selectHourlyWeather() {
+	document
+		.getElementById('hourly-forecast-wrapper')
+		.classList.remove('hidden');
+	document.getElementById('current-weather-wrapper').classList.add('hidden');
+	document
+		.getElementById('one-week-forecast-wrapper')
+		.classList.add('hidden');
+	// console.log('Show just the hourly weather.');
+}
+
+/////////////////////// display One Week Weather- and hide Current and Hourly Weather:
+function selectWeekWeather() {
+	document
+		.getElementById('one-week-forecast-wrapper')
+		.classList.remove('hidden');
+	document.getElementById('current-weather-wrapper').classList.add('hidden');
+	document.getElementById('hourly-forecast-wrapper').classList.add('hidden');
+	// console.log('Show just the weeks weather');
+}
+
+///////////////////// adding event listeners to the buttons:
+const getWeatherBtn = document.getElementById('get-weather-button');
+const currWeatherBtn = document.getElementById('current-weather-btn');
+const hourWeatherBtn = document.getElementById('hourly-weather-btn');
+const weekWeatherBtn = document.getElementById('week-weather-btn');
+
+getWeatherBtn.addEventListener('click', function () {
+	showForecastButtons();
+});
+getWeatherBtn.addEventListener('click', function () {
+	displayWeather();
+});
+currWeatherBtn.addEventListener('click', function () {
+	selectCurrentWeather();
+});
+hourWeatherBtn.addEventListener('click', function () {
+	selectHourlyWeather();
+});
+weekWeatherBtn.addEventListener('click', function () {
+	selectWeekWeather();
+});
